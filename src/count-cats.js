75cdad05abb414fdc3d868b9aa4687ca9349e177
Pavel Kotski <pavelkotsky@mail.ru>
2022-10-17 15:06:20 +0300

    init: start task js-basic

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(cats) {
+  if (cats === undefined || cats.length == 0) {
+      return 0;
+  }
+
+  const stringForSearching = "^^";
+  let countOfCats = 0;
+
+  for (const cat of cats) {
+      if (cat.includes(stringForSearching)) { 
+          countOfCats++;
+          continue;
+      }
+  }
+
+  return countOfCats;
 }
 
 module.exports = {
